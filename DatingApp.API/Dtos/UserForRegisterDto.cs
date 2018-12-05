d121ae282941f51e7015a692a37094da2d0782e1
woaitang131426 <87383325@qq.com>
2018-12-05 15:07:01 +1300

    #1 password validation

--- DatingApp.API/Dtos/UserForRegisterDto.cs
+++ DatingApp.API/Dtos/UserForRegisterDto.cs
@@ -1,7 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace DatingApp.API.Dtos {
     public class UserForRegisterDto {
+
+        [Required]
         public string Username { get; set; }
 
+        [Required]
+        [StringLength (8, MinimumLength = 4, ErrorMessage = "Password are must between 4-8 characters")]
         public string Password { get; set; }
     }
 }
