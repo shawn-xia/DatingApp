0ab916f1b79018892c3ecf17eda1cb83f9ca2c84
woaitang131426 <87383325@qq.com>
2018-12-16 10:21:37 +1300

    #4 create appRoutes, and put it into appmodule

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 import { BsDropdownModule } from 'ngx-bootstrap';
 
 import { AppComponent } from './app.component';
@@ -11,14 +12,27 @@ import { AuthService } from './_services/auth.service';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
 import { ErrorInterceptorProvider } from './_services/error.interceptor';
+import { MessagesComponent } from './messages/messages.component';
+import { ListsComponent } from './lists/lists.component';
+import { MemberListComponent } from './member-list/member-list.component';
+import { appRoutes } from './routes';
 
 @NgModule({
-  declarations: [AppComponent, NavComponent, HomeComponent, RegisterComponent],
+  declarations: [
+    AppComponent,
+    NavComponent,
+    HomeComponent,
+    RegisterComponent,
+    MessagesComponent,
+    ListsComponent,
+    MemberListComponent
+  ],
   imports: [
     BrowserModule,
     HttpClientModule,
     FormsModule,
-    BsDropdownModule.forRoot()
+    BsDropdownModule.forRoot(),
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [AuthService, ErrorInterceptorProvider, AlertifyService],
   bootstrap: [AppComponent]
---
+++ DatingApp-SPA/src/app/routes.ts
@@ -0,0 +1,13 @@
+import { ListsComponent } from './lists/lists.component';
+import { MessagesComponent } from './messages/messages.component';
+import { MemberListComponent } from './member-list/member-list.component';
+import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+
+export const appRoutes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'members', component: MemberListComponent },
+  { path: 'messages', component: MessagesComponent },
+  { path: 'lists', component: ListsComponent },
+  { path: '**', redirectTo: 'home', pathMatch: 'full' }
+];
