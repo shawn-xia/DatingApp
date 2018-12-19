4c6186484b4fda6396333ffe0d7cc70fb8da5074
woaitang131426 <87383325@qq.com>
2018-12-19 19:34:14 +1300

    #2 add resolver

--- DatingApp-SPA/src/app/members/member-list/member-list.component.ts
+++ DatingApp-SPA/src/app/members/member-list/member-list.component.ts
@@ -1,3 +1,4 @@
+import { ActivatedRoute } from '@angular/router';
 import { AlertifyService } from './../../_services/alertify.service';
 import { UserService } from './../../_services/user.service';
 import { Component, OnInit } from '@angular/core';
@@ -13,21 +14,22 @@ export class MemberListComponent implements OnInit {
 
   constructor(
     private userService: UserService,
-    private alertify: AlertifyService
+    private alertify: AlertifyService,
+    private route: ActivatedRoute
   ) {}
 
   ngOnInit() {
-    this.LoadUsers();
+    this.route.data.subscribe(data => (this.users = data['users']));
   }
 
-  LoadUsers() {
-    this.userService.getUsers().subscribe(
-      (users: User[]) => {
-        this.users = users;
-      },
-      error => {
-        this.alertify.error(error);
-      }
-    );
-  }
+  // LoadUsers() {
+  //   this.userService.getUsers().subscribe(
+  //     (users: User[]) => {
+  //       this.users = users;
+  //     },
+  //     error => {
+  //       this.alertify.error(error);
+  //     }
+  //   );
+  // }
 }
